103fcadcedee9fcc321c028b64649dac9123bb73
HelloOnion <HelloOnion@users.noreply.github.com>
2020-07-18 02:09:59 +0900

    big update
    main menu done
    loading screen done
    map done

---
+++ Zombie Hack n Slash/Assets/Scripts/other/SceanManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceanManager : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public GameObject menuBox;
+    public Slider loadingBar;
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadScene(sceneIndex));
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator LoadScene(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Additive);
+
+        menuBox.SetActive(false);
+        loadingScreen.SetActive(true);
+
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            loadingBar.value = progress;
+
+            yield return null;
+        }
+    }
+}
